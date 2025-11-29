24e27e789d2417ed7208c639015a97f8423c6469
KolManis <nikola.ilyin03@yandex.ru>
2025-11-29 21:04:17 +0500

    add createComment, PushCommentToQueue, ConnectProducer

---
+++ producer/producer.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/IBM/sarama"
+	"github.com/gin-gonic/gin"
+)
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func main() {
+	router := gin.Default()
+
+	api := router.Group("/api/v1")
+	{
+		api.POST("/comments", createComment)
+	}
+
+	router.Run(":3000")
+}
+
+func ConnectProducer(brokersUrl []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+
+	producer, err := sarama.NewSyncProducer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return producer, nil
+}
+
+func PushCommentToQueue(topic string, message []byte) error {
+	brokersUrl := []string{"localhost:29092"} // исправлены скобки
+	producer, err := ConnectProducer(brokersUrl)
+	if err != nil {
+		return err
+	}
+	defer producer.Close() // важно закрывать producer
+
+	// Создаем сообщение для Kafka
+	kafkaMessage := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.ByteEncoder(message),
+	}
+
+	// Отправляем сообщение
+	partition, offset, err := producer.SendMessage(kafkaMessage)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Message sent to partition %d, offset %d", partition, offset)
+	return nil
+}
+
+func createComment(c *gin.Context) {
+	var comment Comment
+	if err := c.ShouldBindJSON(comment); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"message": err,
+		})
+		return
+	}
+
+	commentInBytes, err := json.Marshal(comment)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"message": "Error encoding comment",
+		})
+		return
+	}
+
+	if err := PushCommentToQueue("comments", commentInBytes); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"message": "Failed to push to queue",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "Comment pushed successfully",
+		"comment": comment,
+	})
+}
